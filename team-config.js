479653d05a6f110cb14df19c67f1bacb1266b212
arbatova16-sketch <arbatova16@gmail.com>
2026-07-16 07:53:48 +0300

    Update team-config.js

--- team-config.js
+++ team-config.js
@@ -1,5 +1,5 @@
 window.TEAM_CONFIG = {
   enabled: true,
-  supbaseUrl: "https://aovwqxerlovxlaiulkug.supabase.co",
-  supbaseAnonKey: "sb_publishable_KzqzqiHG5nmZIj_ki0eBrQ_lscc12Po"
+  supabaseUrl: "https://aovwqxerlovxlaiulkug.supabase.co",
+  supabaseAnonKey: "sb_publishable_KzqzqiHG5nmZIj_ki0eBrQ_lscc12Po"
 };
